937b460a8e75bb57a24706d5c59b7ae8cb7c538e
david Huang AMP <david_huang@amp.com.au>
2022-09-03 18:14:34 +1000

    init

---
+++ Mqconsumer/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace consum;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory
+        {
+            Uri = new Uri("amqp://guest:guest@localhost:5672")  //call docker container rabbitmq
+        };
+
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel(); //create channel
+
+        channel.QueueDeclare("demo-queue",
+        durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+        //consumer message
+
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received+=(sender, e)=>{
+            var body=e.Body.ToArray();
+            var message=Encoding.UTF8.GetString(body);
+            Console.WriteLine(message);
+        };
+
+        channel.BasicConsume("demo-queue", true,consumer);
+Console.ReadLine();
+}
+}
---
+++ rabbitclient/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+
+ 
+
+
+var factory=new ConnectionFactory{
+    Uri=new Uri("amqp://guest:guest@localhost:5672")  //call docker container rabbitmq
+};
+
+using var connection=factory.CreateConnection();
+using var channel=connection.CreateModel(); //create channel
+
+channel.QueueDeclare("demo-queue", 
+durable:true, exclusive:false, autoDelete: false, arguments: null);
+
+var message=new {Name="producer", message="hello producer"};
+var body=Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+channel.BasicPublish("","demo-queue", null, body); //pass to MQ
+
+
